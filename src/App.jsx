5bb981f8fcd3e786f82c52a0df55ee8104635a82
Malak Elsayed <malakassayed@gmail.com>
2025-06-21 19:04:29 +0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import './Style/App.css'
+import Header from "./Components/Header.jsx";
+import Footer from "./Components/Footer.jsx";
+import Note from "./Components/Note.jsx";
+import CreateArea from "./Components/CreateArea.jsx";
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
